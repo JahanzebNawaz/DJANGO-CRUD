a38eaa3e8cf9d13fdcff368848909e4c5e136b93
jahanzebnawaz <jahanzaib.ch9996@gmail.com>
2019-11-07 17:52:21 +0500

    Project

--- PROJECT/PROJECT/settings.py
+++ PROJECT/PROJECT/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1#r(z)ki1*$&52dmx8&8t8047rg51=-gq4n9u&bfdrwaclp=-4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
